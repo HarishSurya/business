f92a6beb3522258260b568ff4928c2b220fa0ae7
HarishSurya <38115933+HarishSurya@users.noreply.github.com>
2023-09-14 18:58:24 +0530

    Update header.tsx

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
               </svg>
             </Link>
             </div>
-  <div className="font-medium text-purple-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">VIRTUOSO</div>
+  <div className="font-large text-purple-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">VIRTUOSO</div>
           
           {/* Desktop navigation */}
           <nav className="hidden md:flex md:grow">
